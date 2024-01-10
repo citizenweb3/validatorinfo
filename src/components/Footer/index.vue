d4a547570acf92495d91066cfd7817239cadcfed
NRaykov <nediotenev@gmail.com>
2024-01-10 20:46:27 +0200

    Edit meta data in index.html. Edit footer component

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -2,48 +2,32 @@
   <div class="flex flex-wrap items-center md:justify-between w-full">
     <div class="xl:flex-6 xl:max-w-1/2 mx-auto md:mx-0">
       <div class="text-center lg:text-left text-muted">
-        © 2022
-        <a
-          href="#"
-          class="font-semibold ml-1 text-indigo-410 hover:text-indigo-410-active"
-          target="_blank"
-          >LTV</a
-        >
-        &amp;
-        <a
-          href="#"
-          class="font-semibold ml-1 text-indigo-410 hover:text-indigo-410-active"
-          target="_blank"
-          >Creative Tim</a
-        >
+        © 2024
+        <!-- <a href="#" class="font-semibold ml-1 text-indigo-410 hover:text-indigo-410-active" target="_blank">LTV</a> -->
+        <!-- <a href="#" class="font-semibold ml-1 text-indigo-410 hover:text-indigo-410-active" target="_blank">Creative
+          Tim</a> -->
       </div>
     </div>
     <div class="xl:flex-6 xl:max-w-1/2 mx-auto md:mx-0">
       <div class="flex flex-wrap mb-0 pl-0 justify-center lg:justify-end gap-x-6 gap-y-2">
         <div class="">
-          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
+          <!-- <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
             >LTV</a
-          >
+          > -->
         </div>
         <div class="">
-          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
+          <!-- <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
             >Creative Tim</a
-          >
+          > -->
         </div>
         <div class="">
-          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
-            >About Us</a
-          >
+          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank">About Us</a>
         </div>
         <div class="">
-          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
-            >Blog</a
-          >
+          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank">Blog</a>
         </div>
         <div class="">
-          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank"
-            >MIT License</a
-          >
+          <a href="#" class="text-sm text-dark-lighter hover:text-indigo-410" target="_blank">MIT License</a>
         </div>
       </div>
     </div>
