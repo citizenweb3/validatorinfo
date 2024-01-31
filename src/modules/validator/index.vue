ccf2e21e62155952af7fe803349b5c2dd9e01b44
Antonymous <5652120-antonymous@users.noreply.gitlab.com>
2024-01-31 14:35:54 +0200

    fix: validatorId to be part of setup

--- src/modules/validator/index.vue
+++ src/modules/validator/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Validator ID: {{ $route.params.id }}</h1>
+  <h1>Validator ID: {{ validatorId }}</h1>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+// Table
+import ValidatorsTable from './components/ValidatorsTable.vue'
+const route = useRoute()
+
+export default defineComponent({
+  name: 'ValidatorsDashboard',
+  components: {
+    ValidatorsTable,
+  },
+  setup() {
+    return {
+      validatorId: route.params.id,
+    }
+  },
+})
+</script>
