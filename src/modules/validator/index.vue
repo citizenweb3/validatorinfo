c9e1948d3f1eeb2d23bd7d580848446b98345f4d
Antonymous <5652120-antonymous@users.noreply.gitlab.com>
2024-01-31 14:39:59 +0200

    fix: build fix

--- src/modules/validator/index.vue
+++ src/modules/validator/index.vue
@@ -4,15 +4,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
-// Table
-import ValidatorsTable from './components/ValidatorsTable.vue'
 const route = useRoute()
 
 export default defineComponent({
   name: 'ValidatorsDashboard',
-  components: {
-    ValidatorsTable,
-  },
   setup() {
     return {
       validatorId: route.params.id,
