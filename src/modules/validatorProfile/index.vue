fc6f947541aca184190cde110f4b4d68942c6445
Antonymous <5652120-antonymous@users.noreply.gitlab.com>
2024-03-27 00:18:59 +0200

    feat: Network tabs implemented with full layout and mock charts feat: validator links chore: common styles moved to index.scss chore: added screenshot of charts for mocking

--- src/modules/validatorProfile/index.vue
+++ src/modules/validatorProfile/index.vue
@@ -9,8 +9,8 @@ const validatorNetworkId =  computed(() => route.params.validatorNetworkId);
 
 <template>
   <div>
-    <div class='title'>Validator Profile: {{validatorId}}</div>
-    <div class='flex center-items flex-row justify-space-between'>
+    <div class='title'>Validator Profile: <span class='capitalize'>{{validatorId}}</span></div>
+    <div class='flex center-items flex-row justify-space-between mt-5'>
       <div class='flex text-white w-1/3'>
         Embracing Decentralization,
         Empowering Communities.
@@ -22,13 +22,15 @@ const validatorNetworkId =  computed(() => route.params.validatorNetworkId);
       </div>
       <div class='flex flex-col center-items w-1/3'>
         <h2 class='block'>Merits:</h2>
-        <div class='text-white block'>
-          <button class='border-white'>Claimed</button>
-        </div>
+        <button
+          class="bg-blackOlive px-2 border border-gradient-apple-to-lust border-b border-b-3 hover w-40"
+        >
+          <span class="bg-transparent text-white">Claimed</span>
+        </button>
       </div>
     </div>
   </div>
-  <div class="tabs-routes tabs el-tabs is-top is-stretch" v-if='!validatorNetworkId'>
+  <div class="tabs-routes tabs el-tabs is-top is-stretch mt-5" v-if='!validatorNetworkId'>
     <router-link class="el-tabs__item" :to="{name: 'ValidatorRevenue'}">Revenue</router-link>
     <router-link class="el-tabs__item" :to="{name: 'ValidatorMetrics'}">Metrics</router-link>
     <router-link class="el-tabs__item" :to="{name: 'ValidatorNetworksTable'}">Network Table</router-link>
