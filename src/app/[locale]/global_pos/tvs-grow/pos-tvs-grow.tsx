3f7660590ddf29497d1e764134a861071287ea28
Юлия Медведева <60058448+julmedvedeva@users.noreply.github.com>
2024-08-28 13:37:47 +0300

    fix build

--- src/app/[locale]/global_pos/tvs-grow/pos-tvs-grow.tsx
+++ src/app/[locale]/global_pos/tvs-grow/pos-tvs-grow.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
 
-import Doughnut from '@/components/charts/doughnut/doughnut-chart';
-import PageSubTitle from '@/components/common/page-subtitle';
+import Doughnut from '../../../components/charts/doughnut/doughnut-chart';
 
 const PosTvsGrow: FC = () => {
   return (
     <div>
-      <PageSubTitle text="TVS Growt" />
       <div className="flex justify-center">
         <Doughnut />
       </div>
