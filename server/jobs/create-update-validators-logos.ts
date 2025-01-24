1822a381b1c162fc9bd47a7c46ca00a6cfe493f8
stribulsergey <ssargiuz@gmail.com>
2025-01-24 15:07:10 -0300

    mark validators without icons

--- server/jobs/create-update-validators-logos.ts
+++ server/jobs/create-update-validators-logos.ts
@@ -5,11 +5,17 @@ import db from '@/db';
 
 const isIdentityValid = (identity: string): boolean => !!(identity && identity.match(/^[0-9A-Z]{16}$/));
 
-const getLogoFromKeybase = async (identity: string) => {
+const getLogoFromKeybase = async (identity: string): Promise<string | undefined> => {
   try {
-    const username = (
-      await fetch(`https://keybase.io/_/api/1.0/key/fetch.json?pgp_key_ids=${identity}`).then((data) => data.json())
-    ).keys[0].username;
+    const resp = await fetch(`https://keybase.io/_/api/1.0/key/fetch.json?pgp_key_ids=${identity}`).then((data) =>
+      data.json(),
+    );
+
+    if (resp.status.code === 901) {
+      return;
+    }
+
+    const username = resp.keys[0].username;
 
     if (username) {
       return (
@@ -25,17 +31,17 @@ const updateValidatorLogo = async (identity: string, moniker: string) => {
   const logo = await getLogoFromKeybase(identity);
   try {
     await db.validator.upsert({
-      create: { identity: identity, url: logo, moniker: moniker },
-      update: { identity: identity, url: logo },
+      create: { identity: identity, url: logo, moniker: moniker, wrongKey: !logo },
+      update: { identity: identity, url: logo, wrongKey: !logo },
       where: { identity: identity },
     });
   } catch (e) {}
 };
 
 const createUpdateValidatorsLogos = async () => {
-  let validators: Validator[] = (await db.validator.findMany({ orderBy: { moniker: 'asc' } })).filter(
-    (data: Validator) => isIdentityValid(data.identity),
-  );
+  let validators: Validator[] = (
+    await db.validator.findMany({ where: { wrongKey: { not: true } }, orderBy: { moniker: 'asc' } })
+  ).filter((data: Validator) => isIdentityValid(data.identity));
 
   for (const { identity, moniker, url } of validators) {
     let isNeedUpdate = false;
