61ec337459aa1f88b5b99d0f93ff132e689ac786
m1amgn <medvedev.mgn@gmail.com>
2025-04-03 19:30:45 +0500

    #282 made hidden sub-description in home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,11 @@
-import { NextPage } from 'next';
-
 import Validators from '@/app/main-validators/validator-list/validators';
 import TabList from '@/components/common/tabs/tab-list';
 import { mainTabs } from '@/components/common/tabs/tabs-data';
 import { SortDirection } from '@/server/types';
+import { NextPageWithLocale } from '@/i18n';
+import { getTranslations } from 'next-intl/server';
+import PageTitle from '@/components/common/page-title';
+import SubDescription from '@/components/sub-description';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
@@ -14,7 +16,8 @@ interface PageProps {
 
 const defaultPerPage = 25;
 
-const Home: NextPage<PageProps> = async ({ searchParams: q }) => {
+const Home: NextPageWithLocale<PageProps> = async ({ params: { locale }, searchParams: q }) => {
+  const t = await getTranslations({ locale, namespace: 'HomePage' });
   const currentPage = parseInt((q.p as string) || '1');
   const validatorsPerPage = q.pp ? parseInt(q.pp as string) : defaultPerPage;
   const ecosystems: string[] = !q.ecosystems ? [] : typeof q.ecosystems === 'string' ? [q.ecosystems] : q.ecosystems;
@@ -24,6 +27,8 @@ const Home: NextPage<PageProps> = async ({ searchParams: q }) => {
   return (
     <div>
       <TabList page="HomePage" tabs={mainTabs} />
+      <PageTitle text={t('title')} />
+      <SubDescription text={t('sub description')} contentClassName={'mx-4 my-2'} />
       <Validators
         page="HomePage"
         sort={{ sortBy, order }}
