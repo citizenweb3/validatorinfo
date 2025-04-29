3160adb5893da523deca6ced34f1eb202691ddf3
m1amgn <medvedev.mgn@gmail.com>
2025-04-29 10:42:46 +0500

    #344 added apy in staking stats table on revenue node passport page

--- src/app/[locale]/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/staking-stats.tsx
+++ src/app/[locale]/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/staking-stats.tsx
@@ -2,9 +2,13 @@ import { getTranslations } from 'next-intl/server';
 import { FC } from 'react';
 
 import StakingStatsItem from '@/app/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/staking-stats-item';
-import { stakingStatsExample } from '@/app/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/stakingStatsExample';
+import {
+  stakingStatsExample,
+} from '@/app/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/stakingStatsExample';
 import TableHeaderItem from '@/components/common/table/table-header-item';
 import { PagesProps } from '@/types';
+import StakingStatsApyItem
+  from '@/app/validators/[id]/[operatorAddress]/revenue/stacking-stats-table/staking-stats-apy-item';
 
 interface OwnProps extends PagesProps {
   locale: string;
@@ -17,17 +21,18 @@ const StakingStats: FC<OwnProps> = async ({ page, locale }) => {
     <div className="mb-4 mt-2">
       <table className="w-full table-auto border-separate border-spacing-y-2">
         <thead>
-          <tr className="bg-table_header">
-            <th className="bg-background"></th>
-            <TableHeaderItem page={page} name="Yesterday" />
-            <TableHeaderItem page={page} name="Last Week" />
-            <TableHeaderItem page={page} name="Last Month" />
-            <TableHeaderItem page={page} name="Last Year" />
-          </tr>
+        <tr className="bg-table_header">
+          <th className="bg-background"></th>
+          <TableHeaderItem page={page} name="Yesterday" />
+          <TableHeaderItem page={page} name="Last Week" />
+          <TableHeaderItem page={page} name="Last Month" />
+          <TableHeaderItem page={page} name="Last Year" />
+        </tr>
         </thead>
         <tbody>
-          <StakingStatsItem title={t('validator')} data={stakingStatsExample.validator} />
-          <StakingStatsItem title={t('network')} data={stakingStatsExample.network} />
+        <StakingStatsItem title={t('validator')} data={stakingStatsExample.validator} />
+        <StakingStatsItem title={t('network')} data={stakingStatsExample.network} />
+        <StakingStatsApyItem title={'APY'} data={stakingStatsExample.apy} />
         </tbody>
       </table>
     </div>
