8c51787760fa9dd6bf56fa91bb843cba91622e94
m1amgn <medvedev.mgn@gmail.com>
2025-05-12 20:22:30 +0500

    fix bech32prefix in chain params

--- server/indexer.ts
+++ server/indexer.ts
@@ -83,10 +83,11 @@ const runServer = async () => {
     spawnTask(task.name, chains).catch((e) => logError(`Initial run error for task ${task.name}:`, e));
   });
 
-  // Initial run for validatorInfo task after 5 minutes for wait validators updated
+  // Initial run for validatorInfo and slashing-nodes-infos tasks after 5 minutes for wait validators updated
   setTimeout(
     () => {
       spawnTask('validatorInfo', chains).catch((e) => logError(`Initial run error for task validatorInfo:`, e));
+      spawnTask('slashing-nodes-infos', chains).catch((e) => logError(`Initial run error for task slashing-nodes-infos:`, e));
     },
     5 * 60 * 1000,
   );
