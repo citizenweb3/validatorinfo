fdc62a4533c6ec0871870c396acd28a67199e8bb
m1amgn <medvedev.mgn@gmail.com>
2025-09-25 19:58:23 +0700

    made api route for monitor page

--- src/app/services/validator-service.ts
+++ src/app/services/validator-service.ts
@@ -279,6 +279,49 @@ const getRandom = async (ecosystems: string[], take: number): Promise<{ validato
   return { validators };
 };
 
+const getByIdentityWithDetails = async (identity: string) => {
+  return db.validator.findUnique({
+    where: { identity },
+    select: {
+      identity: true,
+      moniker: true,
+      nodes: {
+        select: {
+          operatorAddress: true,
+          jailed: true,
+          delegatorShares: true,
+          moniker: true,
+          identity: true,
+          rate: true,
+          outstandingRewards: true,
+          delegatorsAmount: true,
+          missedBlocks: true,
+          uptime: true,
+          chain: {
+            select: {
+              chainId: true,
+              name: true,
+              prettyName: true,
+              params: {
+                select: {
+                  denom: true,
+                  minimalDenom: true,
+                  coinDecimals: true,
+                },
+              },
+              prices: {
+                orderBy: { createdAt: 'desc' },
+                take: 1,
+                select: { value: true, createdAt: true },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
 const validatorService = {
   getByIdentity,
   getById,
@@ -289,6 +332,7 @@ const validatorService = {
   getValidatorNodesWithChains,
   upsertValidator,
   getRandom,
+  getByIdentityWithDetails,
 };
 
 export default validatorService;
