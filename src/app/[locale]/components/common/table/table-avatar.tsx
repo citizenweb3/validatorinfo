23ecc56e85655d260593053742bab95fcfbb73b5
m1amgn <medvedev.mgn@gmail.com>
2025-09-28 13:26:00 +0700

    update main page mobile: remove unused cols, redesign validators visualisation

--- src/app/[locale]/components/common/table/table-avatar.tsx
+++ src/app/[locale]/components/common/table/table-avatar.tsx
@@ -14,15 +14,15 @@ interface OwnProps {
 
 const TableAvatar: FC<OwnProps> = ({ icon, name, href, textClassName = '' }) => {
   return (
-    <Link href={href} className="flex md:flex-row flex-col items-center hover:text-highlight mb-4 md:mb-0">
-      <div className="relative md:h-[3.3125rem] md:w-[3.3125rem] h-40 w-40">
+    <Link href={href} className="flex flex-row items-center hover:text-highlight my-4 md:mb-0">
+      <div className="relative md:h-[3.3125rem] md:w-[3.3125rem] h-72 w-72">
         {icon ? (
           <Image
             src={icon}
             alt={name}
             width={40}
             height={40}
-            className="absolute left-1/2 top-1/2 z-10 md:h-10 md:w-10 h-28 w-28 sm:h-24 sm:w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-full group-hover/avatar:shadow-2xl object-contain"
+            className="absolute left-1/2 top-1/2 z-10 md:h-10 md:w-10 h-44 w-44 sm:h-24 sm:w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-full group-hover/avatar:shadow-2xl object-contain"
           />
         ) : (
           <Image
@@ -30,12 +30,12 @@ const TableAvatar: FC<OwnProps> = ({ icon, name, href, textClassName = '' }) => 
             alt={name}
             width={40}
             height={40}
-            className="absolute left-1/2 top-1/2 z-10 md:h-11 md:w-11 h-28 w-28 sm:h-24 sm:w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-full group-hover/avatar:shadow-2xl"
+            className="absolute left-1/2 top-1/2 z-10 md:h-11 md:w-11 h-44 w-44 sm:h-24 sm:w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-full group-hover/avatar:shadow-2xl"
           />
         )}
       </div>
       <Tooltip tooltip={name} direction="top" className="hidden md:block">
-        <div className={`${textClassName} ml-1 overflow-x-hidden break-all whitespace-normal text-ellipsis md:text-nowrap text-center md:text-base sm:text-4xl text-5xl`}>
+        <div className={`${textClassName} ml-1 pr-px break-all whitespace-normal overflow-x-clip text-ellipsis md:text-nowrap text-center md:text-base sm:text-4xl text-6xl`}>
           {name}
         </div>
       </Tooltip>
