afef446b2ad6513fe00a3c2ca1f88cbd017d1c4f
m1amgn <medvedev.mgn@gmail.com>
2025-10-20 17:06:05 +0700

    task #282: added titles in account pages

--- src/app/[locale]/networks/[name]/address/[accountAddress]/tokens/page.tsx
+++ src/app/[locale]/networks/[name]/address/[accountAddress]/tokens/page.tsx
@@ -1,10 +1,20 @@
+import { getTranslations } from 'next-intl/server';
+
 import NotToday from '@/components/common/not-today';
+import PageTitle from '@/components/common/page-title';
 import { NextPageWithLocale } from '@/i18n';
 
 interface PageProps {}
 
-const AccountTokensPage: NextPageWithLocale<PageProps> = async () => {
-  return <NotToday />;
+const AccountTokenPage: NextPageWithLocale<PageProps> = async ({ params: { locale } }) => {
+  const t = await getTranslations({ locale, namespace: 'AccountPage.Tokens' });
+
+  return (
+    <div>
+      <PageTitle text={t('title')} />
+      <NotToday />
+    </div>
+  );
 };
 
-export default AccountTokensPage;
+export default AccountTokenPage;
