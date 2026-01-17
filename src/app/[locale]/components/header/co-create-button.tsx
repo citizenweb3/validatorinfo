997e4146bd6c0b215efb83644591bfdb297d7849
m1amgn <medvedev.mgn@gmail.com>
2026-01-17 14:50:00 +0700

    fix: changed support modal window

--- src/app/[locale]/components/header/co-create-button.tsx
+++ src/app/[locale]/components/header/co-create-button.tsx
@@ -34,7 +34,7 @@ const CoCreateButton = () => {
       onClose={handleClose}
       className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
     >
-      <div className="w-max max-w-[80vw] space-y-4 p-10 pt-5">
+      <div className="w-max max-w-[80vw] p-10 pt-5">
         <p className="text-base">
           {t.rich('CoCreate.tagline', {
             github: (chunks) => (
@@ -42,7 +42,10 @@ const CoCreateButton = () => {
             ),
           })}
         </p>
-        <p className="text-base">
+        <p className="text-base mb-5">
+          {t('CoCreate.mission')}
+        </p>
+        <p className="text-base mb-5">
           {t.rich('CoCreate.description', {
             validator: (chunks) => (
               <TextLink href="https://www.citizenweb3.com/" content={chunks} target="_blank" />
@@ -54,8 +57,8 @@ const CoCreateButton = () => {
         </p>
 
         <div>
-          <p className="mb-2 text-base">{t('CoCreate.lookingFor')}</p>
-          <ul className="space-y-2 pl-2">
+          <p className="mb-4 text-base">{t('CoCreate.lookingFor')}</p>
+          <ul className="space-y-2 pl-2 mb-5">
             <ListItem>{t('CoCreate.partners')}</ListItem>
             <ListItem>{t('CoCreate.interns')}</ListItem>
             <ListItem>{t('CoCreate.delegators')}</ListItem>
@@ -64,16 +67,14 @@ const CoCreateButton = () => {
 
         <p className="text-base">
           {t.rich('CoCreate.telegram', {
-            link: (chunks) => <TextLink href="https://t.me/citizenweb3" content={chunks} target="_blank" />,
+            tgLink: (chunks) => <TextLink href="https://t.me/citizenweb3" content={chunks} target="_blank" />,
           })}
         </p>
 
         <p className="text-base">
-          <TextLink
-            href="https://staking.citizenweb3.com/"
-            content={t('CoCreate.support')}
-            target="_blank"
-          />
+          {t.rich('CoCreate.support', {
+            stakingLink: (chunks) => <TextLink href="https://staking.citizenweb3.com/" content={chunks} target="_blank" />,
+          })}
         </p>
       </div>
     </BaseModal>
